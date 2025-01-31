f6699d18dd1054f17f1d4dcf1dfb2ee91c842670
quiesscent <ephesianslewis@gmail.com>
2025-01-31 08:30:52 +0300

    minors

--- backend/core/credentials.py
+++ backend/core/credentials.py
@@ -15,7 +15,6 @@ class MpesaAccessToken:
         consumer_key = os.getenv("CONSUMER_KEY")
         consumer_secret = os.getenv("CONSUMER_SECRET")
         api_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-
         response = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
         json_response = response.json()
         return json_response['access_token']
@@ -28,7 +27,7 @@ class LipanaMpesaPpassword:
     # Daraja Sandbox credentials
     Business_short_code = os.getenv("BUSINESS_SHORT_CODE")
     Shortcode_password = os.getenv("PASSKEY")
-    
+
     @staticmethod
     def get_lipa_time():
         return datetime.datetime.now().strftime('%Y%m%d%H%M%S')
