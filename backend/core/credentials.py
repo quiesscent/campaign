35547103f6e91814afa7840a2fb36ef8ca25274f
quiesscent <ciphedc@gmail.com>
2025-02-25 23:19:42 +0300

    berzerk

--- backend/core/credentials.py
+++ backend/core/credentials.py
@@ -12,8 +12,8 @@ class MpesaAccessToken:
     @staticmethod
     def get_access_token():
         # Daraja Sandbox credentials
-        consumer_key = os.getenv("CONSUMER_KEY")
-        consumer_secret = os.getenv("CONSUMER_SECRET")
+        consumer_key = 'aZPo66U3mqRyFIevpbhOYNDAy2JB0fvGeJitOG2RIe8IkzSs'
+        consumer_secret = 'p7ev4dnWB6XOs8QRQPuGpQWpgvbkvGAvrTy3nAMUhUk27mpMAL7Wlns69f2qEn2I'
         api_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
         response = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
         json_response = response.json()
@@ -25,8 +25,8 @@ class MpesaAccessToken:
 
 class LipanaMpesaPpassword:
     # Daraja Sandbox credentials
-    Business_short_code = os.getenv("BUSINESS_SHORT_CODE")
-    Shortcode_password = os.getenv("PASSKEY")
+    Business_short_code = 174379
+    Shortcode_password = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
 
     @staticmethod
     def get_lipa_time():
